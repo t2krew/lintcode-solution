b595b2a4e4ec20d91b20c173e15e8fe8b791bb54
t2krew <wanhao.huang@outlook.com>
2017-05-14 22:56:35 +0800

    82.single-number.py

---
+++ python/82.single-number.py
@@ -0,0 +1,18 @@
+# 给出2*n + 1 个的数字，除其中一个数字之外其他每个数字均出现两次，找到这个数字。
+
+# 给出 [1,2,2,1,3,4,3]，返回 4
+
+
+class Solution:
+    """
+    @param A : an integer array
+    @return : a integer
+    """
+    def singleNumber(self, A):
+        # write your code here
+        if A is None or len(A) == 0:
+            return 0
+        ans = A[0]
+        for i in range(1, len(A)):
+            ans ^= A[i]
+        return ans
