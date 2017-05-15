a42562df1e6bf405d9cb2682d71c2b66344b6813
t2krew <wanhao.huang@outlook.com>
2017-05-15 09:57:53 +0800

    102.linked-list-cycle

---
+++ 102.linked-list-cycle.java
@@ -0,0 +1,39 @@
+// 给定一个链表，判断它是否有环。
+
+// 给出 -21->10->4->5, tail connects to node index 1，返回 true
+
+
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @return: True if it has a cycle, or false
+     */
+    public boolean hasCycle(ListNode head) {  
+        // write your code here
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
---
+++ python/102.linked-list-cycle.py
@@ -0,0 +1,29 @@
+# 给定一个链表，判断它是否有环。
+
+# 给出 -21->10->4->5, tail connects to node index 1，返回 true
+
+"""
+Definition of ListNode
+class ListNode(object):
+
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+class Solution:
+    """
+    @param head: The first node of the linked list.
+    @return: True if it has a cycle, or false
+    """
+    def hasCycle(self, head):
+        # write your code here
+        fast = slow = head
+        while(fast is not None and fast and fast.next is not None) :
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                break
+        if fast is None or fast.next is None:
+            return False
+        else:
+            return True
